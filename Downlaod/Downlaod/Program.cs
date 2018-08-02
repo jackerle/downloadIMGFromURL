32218d5111a4882c5cee251be9913ddbd15cfbbb
jackerle <zeustololisis@gmail.com>
2018-08-02 09:55:38 +0700

    first can download all page from url

--- Downlaod/Downlaod/Program.cs
+++ Downlaod/Downlaod/Program.cs
@@ -14,6 +14,8 @@ namespace Downlaod
 {
     class Program
     {
+        int i = 0;
+        WebClient wci = new WebClient();
         static String cutOut(String n)
         {
             int first = n.IndexOf("http");
@@ -30,14 +32,24 @@ namespace Downlaod
             sr.Close();
             res.Close();
             return srccode;
-
+        }
+         void downloads(String dl)
+        {
+            using (WebClient wci = new WebClient())
+            {
+                wci.Headers["Referer"] = "https://webtoon-phinf.pstatic.net";
+                 wci.DownloadFile(dl, "img_" + i + ".jpg");
+                Console.WriteLine("download " + i + "success");
+                i++;
+            }
         }
         static void Main(string[] args)
         {
             using(WebClient wc = new WebClient())
             {
+                Program pg = new Program();
                 wc.Headers["Referer"] = "https://webtoon-phinf.pstatic.net";
-                String src = getSourceCode("https://www.webtoons.com/th/fantasy/return-my-school-life/ep33/viewer?title_no=1355&episode_no=34");
+                String src = getSourceCode("https://www.webtoons.com/th/romance/gangnam-beauty/%E0%B8%95%E0%B8%AD%E0%B8%99%E0%B8%9E%E0%B9%80%E0%B8%A8%E0%B8%A9-ep02/viewer?title_no=792&episode_no=88");
                 /*int last = _src.IndexOf("viewer_ad_area");
                 int first = _src.IndexOf("_imageList");
                 String src = _src.Substring(first,last-first);*/
@@ -50,9 +62,11 @@ namespace Downlaod
                 Match match = rgx.Match(src);
                 if (match.Success)
                 {
-                    Console.WriteLine(cutOut(match.Value));
+                    //Console.WriteLine(cutOut(match.Value));
+                    pg.downloads(cutOut(match.Value));
                     foreach (Match m in rgx.Matches(src, match.Index + match.Length))
-                        Console.WriteLine(cutOut(m.Value));
+                        //Console.WriteLine(cutOut(m.Value));
+                        pg.downloads(cutOut(m.Value));
                 }
                 
                 
